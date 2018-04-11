521e8259d06d7ce28a26ffb6a495edb5d9c82252
Sebastián Villamonte <walden2av@gmail.com>
2018-04-11 19:17:44 +1200

    Modified the function of registering request so that the request is saved to a queue.

--- register-request/index.js
+++ register-request/index.js
@@ -1,17 +1,8 @@
-module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+module.exports = function (context) {
+  context.log('Incoming request processed');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-    context.done();
-};
+  context.bindings.incomingRequest = 'New incoming request';
+  context.res = { status: 200 };
+  
+  context.done();
+}
