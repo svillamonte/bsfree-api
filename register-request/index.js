e911ad7a6141b26f8bc635a97820d1555b9e6ff1
Sebastián Villamonte <walden2av@gmail.com>
2018-04-11 21:12:15 +1200

    Test change to try releasing it with VSTS.

--- register-request/index.js
+++ register-request/index.js
@@ -1,8 +1,11 @@
 module.exports = function (context) {
-  context.log('Incoming request processed');
+  context.log('Incoming request registered');
 
   context.bindings.incomingRequest = 'New incoming request';
-  context.res = { status: 200 };
+  context.res = { 
+    status: 200, 
+    message: 'All good' 
+  };
   
   context.done();
 }
