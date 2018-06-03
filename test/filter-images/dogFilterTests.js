210196d082f57bf63c6937dc9948d5d8ff6486ef
Sebastián Villamonte <walden2av@gmail.com>
2018-06-03 16:47:09 +1200

    Added more UT to the git filter module.

--- test/filter-images/dogFilterTests.js
+++ test/filter-images/dogFilterTests.js
@@ -2,14 +2,50 @@ const assert = require('assert');
 const isDog = require('../../src/filter-images/dogFilter');
 
 describe('dogFilter', () => {
-  describe('isDog', () => {
-    it('should return false', () => {
+  describe('#isDog()', () => {
+    it('returns false if no tags', () => {
       const data = {
         tags: []
       };
 
       const result = isDog(data);
       assert.equal(result, false);
+    });
+
+    it('returns false if dog tag is below threshold', () => {
+      const data = {
+        tags: [
+          {
+            name: 'something',
+            confidence: 0.9
+          },
+          {
+            name: 'dog',
+            confidence: 0.3
+          }
+        ]
+      };
+
+      const result = isDog(data);
+      assert.equal(result, false);
+    });
+
+    it('returns true if dog tag is above threshold', () => {
+      const data = {
+        tags: [
+          {
+            name: 'something',
+            confidence: 0.9
+          },
+          {
+            name: 'dog',
+            confidence: 0.51
+          }
+        ]
+      };
+
+      const result = isDog(data);
+      assert.equal(result, true);
     });
   });
 });
