9df49170c81846c14fb5ac24a1e8b5bb255511fc
Sebastián Villamonte <walden2av@gmail.com>
2018-08-14 20:32:55 +1200

    Followed styling guidelines for the common module.

--- src/common/filteredShoutStorage.js
+++ src/common/filteredShoutStorage.js
@@ -3,18 +3,25 @@ import azure from 'azure-storage';
 
 const TABLE_NAME = 'FilteredShouts';
 const tableService = azure.createTableService(
-  AZURE_STORAGE_ACCOUNT, 
+  AZURE_STORAGE_ACCOUNT,
   AZURE_STORAGE_KEY
 );
 
+/**
+ * Gets the filtered shout from storage
+ * @param {string} rowKey
+ * @return {Promise}
+ */
 function getByRowKey(rowKey) {
   const { TableQuery, TableUtilities } = azure;
 
   const query = new TableQuery().where(
     TableQuery.stringFilter(
-      'RowKey', 
-      TableUtilities.QueryComparisons.EQUAL, 
-      rowKey));
+      'RowKey',
+      TableUtilities.QueryComparisons.EQUAL,
+      rowKey
+    )
+  );
 
   const promise = new Promise((resolve, reject) => {
     const callback = (error, result) => {
@@ -28,16 +35,19 @@ function getByRowKey(rowKey) {
   return promise;
 }
 
-const insertShout = (shoutEntity) => new Promise((resolve, reject) => {
-  const callback = (error) => {
-    if (error) reject(error);
-    resolve();
-  };
+/**
+ * Saves shout into storage
+ * @param {object} shoutEntity
+ * @return {Promise}
+ */
+const insertShout = (shoutEntity) =>
+  new Promise((resolve, reject) => {
+    const callback = (error) => {
+      if (error) reject(error);
+      resolve();
+    };
 
-  tableService.insertEntity(TABLE_NAME, shoutEntity, callback);
-});
+    tableService.insertEntity(TABLE_NAME, shoutEntity, callback);
+  });
 
-export {
-  getByRowKey,
-  insertShout
-};
+export { getByRowKey, insertShout };
--- src/common/settings.js
+++ src/common/settings.js
@@ -1,5 +1,5 @@
 export default {
   COGNITIVE_SERVICES_KEY: process.env['CognitiveServicesKey'],
   AZURE_STORAGE_KEY: process.env['AzureStorageKey'],
-  AZURE_STORAGE_ACCOUNT: process.env['AzureStorageAccount']
+  AZURE_STORAGE_ACCOUNT: process.env['AzureStorageAccount'],
 };
