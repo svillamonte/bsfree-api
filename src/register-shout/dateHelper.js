ec45c38d18ffd16d515292a94831c50cb48d8402
Sebastián Villamonte <walden2av@gmail.com>
2018-10-13 15:04:24 +1300

    Added a polyfill for padStart, as Node version in Azure Functions does not support it.

--- src/register-shout/dateHelper.js
+++ src/register-shout/dateHelper.js
@@ -19,7 +19,28 @@ const getPaddedTicksToMaxDate = () => {
   const now = new Date();
 
   const ticks = maxDate.getTime() - now.getTime();
+
+  padStartPolyfill(); // Azure Functions runtime is outdated.
   return ticks.toString().padStart(16, '0');
+};
+
+/* eslint no-extend-native:0 */
+const padStartPolyfill = () => {
+  if (!String.prototype.padStart) {
+    String.prototype.padStart = function padStart(targetLength, padString) {
+      targetLength = targetLength >> 0; // truncate if number, or convert non-number to 0;
+      padString = String(typeof padString !== 'undefined' ? padString : ' ');
+      if (this.length >= targetLength) {
+        return String(this);
+      } else {
+        targetLength = targetLength - this.length;
+        if (targetLength > padString.length) {
+          padString += padString.repeat(targetLength / padString.length); // append to original to ensure we are longer than needed
+        }
+        return padString.slice(0, targetLength) + String(this);
+      }
+    };
+  }
 };
 
 export { getCurrentDate, getPaddedTicksToMaxDate };
